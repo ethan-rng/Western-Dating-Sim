c1d19c0fa0f509d6992f0fc3e14b1d87f1a7aa81
Jasper Yang <jyan457@uwo.ca>
2024-03-25 23:33:06 +0000

    added title and spaced menu options

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 # Font setup
+font_title = pygame.font.SysFont(None, 100)
 font = pygame.font.SysFont(None, 55)
 
 # Main Menu Items
@@ -25,10 +26,12 @@ menu_items = ['Start New Game', 'Load Game', 'Tutorials', 'High Scores', 'Exit']
 
 def draw_menu():
     screen.fill(BLACK)
+    title = font_title.render('Western Dating Simulator', True, WHITE)
+    screen.blit(title, (screen_width // 2 - title.get_width() // 2, screen_height -  4 * (screen_height // 5)))
     for index, item in enumerate(menu_items):
         menu_text = font.render(item, True, WHITE)
         x = screen_width // 2 - (menu_text.get_width() // 2)
-        y = screen_height // 2 - (menu_text.get_height() * len(menu_items) // 2) + (index * 60)
+        y = screen_height // 2 - (menu_text.get_height() * len(menu_items) // 2) + (index * 90)
         screen.blit(menu_text, (x, y))
     pygame.display.flip()
 
@@ -47,7 +50,7 @@ def main_menu():
                 for index, item in enumerate(menu_items):
                     menu_text = font.render(item, True, WHITE)
                     x = screen_width // 2 - (menu_text.get_width() // 2)
-                    y = screen_height // 2 - (menu_text.get_height() * len(menu_items) // 2) + (index * 60)
+                    y = screen_height // 2 - (menu_text.get_height() * len(menu_items) // 2) + (index * 90)
                     item_rect = menu_text.get_rect(topleft=(x, y))
                     if item_rect.collidepoint(event.pos):
                         menu_click(index)
@@ -56,18 +59,16 @@ def main_menu():
   
 def menu_click(index):
     # This function handles the menu clicks
-    # Main game loop
     if index == 0:
-        print()
-    #takes you to the load game menu
+        print("Start the Game")
     elif index == 1:
-        print()
+        print("Load Game")
     #takes you to the tutorial menu
     elif index == 2:
-        print()
+        print("Tutorials")
     #takes you to the high scores table
     elif index == 3:
-        print()
+        print("High Scores")
     #quits the game
     elif index == 4:
         pygame.quit()
@@ -77,17 +78,3 @@ def menu_click(index):
 # Call the main menu before entering the main game loop
 main_menu()
 
-# Main game loop
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-
-    # Fill the screen with a color (e.g., black)
-    screen.fill((0, 0, 0))
-
-    # Update the display
-    pygame.display.flip()
-
-pygame.quit()
