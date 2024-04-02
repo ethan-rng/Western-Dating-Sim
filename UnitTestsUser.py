336d7bf95c2089b0dd4e9969c691570040bbf7a2
Jasper Yang <jyan457@uwo.ca>
2024-04-02 01:57:40 +0000

    UnitTestsUser.py edited online with Bitbucket
    added pydoc comments

--- UnitTestsUser.py
+++ UnitTestsUser.py
@@ -1,24 +1,36 @@
+"""
+Module: test_user
+Author: Jasper Yang
+This module provides unit tests for the User class.
+
+"""
+
 from models.User import User
-from models.exceptions import DuplicateUser, UserNotFound, IncorrectPassword, AdminLevelAccount, IncorrectPrivilege
+from models.exceptions import DuplicateUser, UserNotFound, IncorrectPassword, AdminLevelAccount
 from hashlib import sha256
-import json, os
+import json
+import os
 import unittest
 
-#unit tests for user Class
-class testUser(unittest.TestCase):
+class TestUser(unittest.TestCase):
+    """
+    Unit tests for the User class.
+    """
 
-    '''tests the create user function'''
-    def test_createUser(self):
-        #create a user
-        Jasper:User = User()
-        Jasper.createUser("Jasper", "abcd")
-        #checks if the username and password are correct
-        self.assertEqual(Jasper.username, "Jasper")
-        self.assertEqual(Jasper._password, sha256("abcd".encode()).hexdigest())
-        #tries to create a duplicate to see if it will be rejected
-        Jasper2:User = User()
-        self.assertRaises(DuplicateUser, Jasper2.createUser, "Jasper", "abcd")
-        #deletes Jasper from password
+    def test_create_user(self):
+        """
+        Tests the create user function.
+        """
+        # Create a user
+        jasper = User()
+        jasper.create_user("Jasper", "abcd")
+        # Checks if the username and password are correct
+        self.assertEqual(jasper.username, "Jasper")
+        self.assertEqual(jasper._password, sha256("abcd".encode()).hexdigest())
+        # Tries to create a duplicate to see if it will be rejected
+        jasper2 = User()
+        self.assertRaises(DuplicateUser, jasper2.create_user, "Jasper", "abcd")
+        # Deletes Jasper from password
         for user in User.Users:
             if user["username"] == "Jasper":
                 User.Users.remove(user)
@@ -26,23 +38,25 @@ class testUser(unittest.TestCase):
         # Update JSON file
         with open(os.path.join("models", "data", "UserPasswords.json"), 'w', encoding='utf-8') as file:
             json.dump(User.Users, file, indent=4)
-        #checks if attempt to create admin level accounts are rejected
-        self.assertRaises(AdminLevelAccount, Jasper2.createUser, "developer", "abcd")
-        self.assertRaises(AdminLevelAccount, Jasper2.createUser, "instructor", "abcd")
+        # Checks if attempt to create admin level accounts are rejected
+        self.assertRaises(AdminLevelAccount, jasper2.create_user, "developer", "abcd")
+        self.assertRaises(AdminLevelAccount, jasper2.create_user, "instructor", "abcd")
 
-    '''test the load User function'''
-    def test_loadUser(self):
-        #creates a user
-        Ethan:User = User()
-        Ethan.createUser("Ethan", "abcd")
-        #loads the user
-        Ethan2:User = User()
-        Ethan2.loadUser("Ethan")
-        self.assertEqual(Ethan2._password, sha256("abcd".encode()).hexdigest())
-        #attempts to load non-existent user
-        Ethan3:User = User()
-        self.assertRaises(UserNotFound, Ethan3.loadUser, "Ethan3")
-        #deletes Ethan from password
+    def test_load_user(self):
+        """
+        Tests the load User function.
+        """
+        # Creates a user
+        ethan = User()
+        ethan.create_user("Ethan", "abcd")
+        # Loads the user
+        ethan2 = User()
+        ethan2.load_user("Ethan")
+        self.assertEqual(ethan2._password, sha256("abcd".encode()).hexdigest())
+        # Attempts to load non-existent user
+        ethan3 = User()
+        self.assertRaises(UserNotFound, ethan3.load_user, "Ethan3")
+        # Deletes Ethan from password
         for user in User.Users:
             if user["username"] == "Ethan":
                 User.Users.remove(user)
@@ -50,24 +64,26 @@ class testUser(unittest.TestCase):
         # Updates the JSON file
         with open(os.path.join("models", "data", "UserPasswords.json"), 'w', encoding='utf-8') as file:
             json.dump(User.Users, file, indent=4)
-    
-    '''Tests the login and logout functions'''
+
     def test_login_and_logout(self):
-        #creates user
-        Jasmine:User = User()
-        Jasmine.createUser("Jasmine", "abcd")
-        #attemp to login with the wrong password
-        Aaron:User = User()
-        Aaron.loadUser("Jasmine")
-        self.assertRaises(IncorrectPassword, Aaron.login,"Jasmine", "a")
-        #login wiht correct password
-        Aaron.login("Jasmine","abcd")
-        self.assertEqual(Aaron.LoggedInUser, Aaron)
-        self.assertEqual(Aaron.LoggedInUser.username, "Jasmine")
-        #logout
-        Aaron.logout()
-        self.assertNotEqual(Aaron.LoggedInUser, Aaron)
-        #deletes Jasmine from password
+        """
+        Tests the login and logout functions.
+        """
+        # Creates user
+        jasmine = User()
+        jasmine.create_user("Jasmine", "abcd")
+        # Attempt to login with the wrong password
+        aaron = User()
+        aaron.load_user("Jasmine")
+        self.assertRaises(IncorrectPassword, aaron.login, "Jasmine", "a")
+        # Login with correct password
+        aaron.login("Jasmine", "abcd")
+        self.assertEqual(aaron.logged_in_user, aaron)
+        self.assertEqual(aaron.logged_in_user.username, "Jasmine")
+        # Logout
+        aaron.logout()
+        self.assertNotEqual(aaron.logged_in_user, aaron)
+        # Deletes Jasmine from password
         for user in User.Users:
             if user["username"] == "Jasmine":
                 User.Users.remove(user)
