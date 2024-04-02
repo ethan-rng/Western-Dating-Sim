24b9901add7353bd3dfe4dbce6527198c6ec963f
Jasper Yang <jyan457@uwo.ca>
2024-04-02 02:50:47 +0000

    added pydoc comments

--- models/HighScoreTable.py
+++ models/HighScoreTable.py
@@ -1,15 +1,42 @@
-import os, json
+"""
+Module: highscore_table_model
+Author Ethan Rong, Aaron XIe, Jasper Yang
+This module provides the HighScoreTable class for managing high scores.
+
+"""
+
+import os
+import json
 from models.exceptions import UserNotFound
 
 
 class HighScoreTable:
+    """
+    A class to represent a high score table.
+
+    Attributes:
+    - scores: a dictionary containing the high scores
+
+    Methods:
+    - __init__: initializes the HighScoreTable instance
+    - getScores: public method to retrieve the high scores from disk
+    - getUserScore: public method to retrieve the score of a specific user
+    - getRankScore: public method to retrieve the score at a specific rank
+    """
+
     def __init__(self) -> None:
+        """
+        Initializes the HighScoreTable instance.
+        """
         self.scores: dict = self.getScores()
 
-    # PUBLIC FACING METHODS
-    """ Public Method which opens the json file and returns the scores dict """
-
     def getScores(self) -> dict:
+        """
+        Public method to retrieve the high scores from disk.
+
+        Returns:
+        - dict: a dictionary containing the high scores
+        """
         scores = {}
         with open(os.path.join('models', 'data', 'UserGameStates.json'), "r") as file:
             scores_list = json.load(file)
@@ -17,9 +44,19 @@ class HighScoreTable:
                 scores[score["username"]] = score["finalScore"]
             return scores
 
-    """ Public Method which allows the instructor to view the progress of a specific user (throws UserNotFound exception) """
-
     def getUserScore(self, username: str) -> int:
+        """
+        Public method to retrieve the score of a specific user.
+
+        Parameters:
+        - username: the username of the user whose score is to be retrieved
+
+        Returns:
+        - int: the score of the specified user
+
+        Raises:
+        - UserNotFound: if the specified user does not exist
+        """
         self.getScores()
 
         for key in self.scores.keys():
@@ -27,10 +64,20 @@ class HighScoreTable:
                 return self.scores[key]
         raise UserNotFound(f"No user found with username {username}")
 
-    """ Public Method which allows the instructor to view the progress of a specific user (throws IndexError exception) """
-    
     def getRankScore(self, rank: int) -> tuple:
-        sorted_scores = sorted(self.scores.items(), key = lambda x: x[1],reverse=True)
-        return sorted_scores[rank-1]
+        """
+        Public method to retrieve the score at a specific rank.
+
+        Parameters:
+        - rank: the rank of the score to be retrieved
+
+        Returns:
+        - tuple: a tuple containing the username and score at the specified rank
+
+        Raises:
+        - IndexError: if the rank is out of bounds
+        """
+        sorted_scores = sorted(self.scores.items(), key=lambda x: x[1], reverse=True)
+        return sorted_scores[rank - 1]
 
 
