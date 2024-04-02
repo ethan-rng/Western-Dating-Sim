c74619eed462144f93b809a514e15e938e3e235a
Jasper Yang <jyan457@uwo.ca>
2024-04-02 02:47:47 +0000

    added pydoc comments

--- models/Player.py
+++ models/Player.py
@@ -1,7 +1,15 @@
+"""
+Module: player_model
+Author: Jasper Yang, Ethan Rong, Aaron Xie
+This module provides the Player class for managing player accounts and game states.
+
+"""
+
 from models.User import User
 from models.exceptions import IllegalStats, UserNotFound
 from typing import List
-import os, json
+import os
+import json
 
 # CONSTANTS
 MAX_SCORE = 10
@@ -10,11 +18,29 @@ CHARACTER_BIAS = {
 }
 
 
-# Player Class
 class Player(User):
+    """
+    A class to represent player accounts and game states.
+
+    Attributes:
+    - Players: class variable representing a list of player data
+
+    Methods:
+    - __init__: initializes the Player instance
+    - createPlayer: public method to create a new player account
+    - loadPlayer: public method to load an existing player account
+    - saveProgress: public method to save player progress
+    - updateStats: public method to update player stats after an interaction
+    - getFinalScore: public method to calculate the final score of the player
+    """
+
     Players: List[dict] = []
 
     def __init__(self) -> None:
+        """
+        Initializes the Player instance.
+        Loads player data from memory (only runs on the first instance the class is called).
+        """
         super().__init__()
         if not Player.Players:
             with open(os.path.join("models", "data", "UserGameStates.json"), "r") as file:
@@ -41,11 +67,20 @@ class Player(User):
             "Serena": 0,
         }
 
-    # PUBLIC FACING METHODS
-    """ Public Method which creates a new player account (throws AdminLevelAccount DuplicateUser, IllegalStats) """
     def createPlayer(self, username: str, password: str, charisma: int, intel: int, attraction: int) -> None:
+        """
+        Public method to create a new player account.
 
-        # Stats for Individual Players
+        Parameters:
+        - username: the username of the new player
+        - password: the password of the new player
+        - charisma: the charisma stat of the new player
+        - intel: the intelligence stat of the new player
+        - attraction: the attraction stat of the new player
+
+        Raises:
+        - IllegalStats: if the sum of charisma, intelligence, and attraction is greater than MAX_SCORE
+        """
         if charisma + intel + attraction > MAX_SCORE or (charisma < 0 or intel < 0 or attraction < 0):
             raise IllegalStats(charisma, intel, attraction)
 
@@ -62,9 +97,16 @@ class Player(User):
             "Serena": 0,
         }
 
-    """ Public Method which allows the player to load from a previous game state (throws UserNotFound exception) """
-
     def loadPlayer(self, username: str) -> None:
+        """
+        Public method to load an existing player account.
+
+        Parameters:
+        - username: the username of the player to load
+
+        Raises:
+        - UserNotFound: if the player does not exist
+        """
         super().loadUser(username)
         with open(os.path.join("models", "data", "UserGameStates.json"), "r") as file:
             jsonData = json.load(file)
@@ -79,8 +121,10 @@ class Player(User):
                 
             raise UserNotFound(username)
 
-    """ Public Method which saves all progress of the player to the disk """
     def saveProgress(self) -> None:
+        """
+        Public method to save player progress.
+        """
         with open(os.path.join("models", "data", "UserGameStates.json"), "w") as file:
             playerData = {
                 "username": self.username,
@@ -94,9 +138,17 @@ class Player(User):
             Player.Players.append(playerData)
             json.dump(Player.Players, file, ensure_ascii=False, indent=4)
 
-    """ Public Method which allows the player to update their stats after an interaction (throws IncorrectPrivilege, KeyError)"""
     def updateStats(self, character: str, newAttractionScore: int) -> int:
-        # Checking If AttractionScore is Maxed Out or Mined Out
+        """
+        Public method to update player stats after an interaction.
+
+        Parameters:
+        - character: the character associated with the interaction
+        - newAttractionScore: the new attraction score obtained from the interaction
+
+        Returns:
+        - int: the updated attraction score for the character
+        """
         if self._attractionScore[character] >= 100 and newAttractionScore > 0:
             print(f"Attraction Score Maxed Out With {character}")
             return 100
@@ -104,7 +156,6 @@ class Player(User):
             print(f"Attraction Score Minimized With {character}")
             return 0
 
-        # Calculating New Attraction Score After Factoring Player Bias
         bias = 0
         for characterBias in CHARACTER_BIAS[character]:
             if characterBias == "charisma":
@@ -117,8 +168,13 @@ class Player(User):
         self._attractionScore[character] += newAttractionScore * (bias * 0.1 * 0.5 + 1)
         return self._attractionScore[character]
 
-    """ Public Method which calculates the final score of the player (Sum of all the individuals scores) """
     def getFinalScore(self) -> int:
+        """
+        Public method to calculate the final score of the player.
+
+        Returns:
+        - int: the final score of the player
+        """
         finalScore = 0
         for score in self._attractionScore.values():
             finalScore += score
