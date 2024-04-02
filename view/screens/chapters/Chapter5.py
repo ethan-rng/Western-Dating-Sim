5553575437ba0dcaa61e016a21b85b931d2a376f
Jasmine Gu <jgu284@uwo.ca>
2024-04-01 20:08:45 -0400

    Chapter 5

--- view/screens/chapters/Chapter5.py
+++ view/screens/chapters/Chapter5.py
@@ -26,12 +26,6 @@ class Chapter5(Chapter):
         self.dialogue_options = {
             1: ["I really like you. Will you be my girlfriend?", "I've had a great time getting to know you, I was wondering if you want to make this official?", "So, I was thinking, we get along pretty well, huh? Maybe we should start going out for real?", "I kinda like you more than just a friend. Do you wanna be my girlfriend?"],
         }
-
-        self.dialogue_responses = {
-            "Yes": ("Yes, I’d love that!", 2,4),
-            "No": ("Oh, sorry. I never thought of you that way. I prefer to be friends...", 3,4),
-        }
-        
         self.draw_chapter5()
 
     def draw_chapter5(self):
@@ -51,20 +45,36 @@ class Chapter5(Chapter):
 
                     # if selected choice index isnt none...
                 if self.selected_choice_index is not None:
-                        # show the selected choice response and background text
-                    selected_option_text = self.dialogue_options[self.current_dialogue_index][self.selected_choice_index]
-                        # Get the response text
-                    response_text, background_index, self.next_background_index = self.dialogue_responses[selected_option_text]
-                        # store the choice made
-                    self.choices_made[self.current_dialogue_index] = selected_option_text
-                    self.show_choices = False
-
-                        # After updating indices, get the new background and draw it
+                                 # Calculating Player's Score This Chapter and Updating It
+                    for choice in self.choices_made.values():
+                        if choice == "I really like you. Will you be my girlfriend?":
+                            self.currPlayer.updateStats("Serena", 0)
+                            print(self.currPlayer.attraction)
+                        elif choice == "I've had a great time getting to know you, I was wondering if you want to make this official?":
+                            self.currPlayer.updateStats("Serena", 15)                                
+                            print(self.currPlayer.attraction)
+                        elif choice == "So, I was thinking, we get along pretty well, huh? Maybe we should start going out for real?":
+                            self.currPlayer.updateStats("Serena", 20)
+                        elif choice == "I kinda like you more than just a friend. Do you wanna be my girlfriend?":
+                            self.currPlayer.updateStats("Serena", 10)
+                        else:
+                            raise Exception("Choice Isn't Valid")
+                        
+                    if self.currPlayer.attraction >= 0:
+                        response_text = "Yes, I’d love that!"
+                        background_index= 2
+                        self.selected_choice_index = 2
+                    else:
+                        response_text = "Oh, sorry. I never thought of you that way. I prefer to be friends..."
+                        background_index = 3
+                        self.selected_choice_index = 1
+       
+                    # After updating indices, get the new background and draw it
                     current_background = self.dialogueImages[background_index]
                     self.screen.blit(current_background, (0, 0))
                     dialogue_box = DialogueBox(self.screen, font_size=50, box_height=200)
                     dialogue_box.draw(response_text)
-                    self.selected_choice_index = None
+    
             pygame.display.flip()          
         
             return ""
@@ -75,11 +85,14 @@ class Chapter5(Chapter):
                 self.checkQuitGame(event)
                 if event.type == pygame.MOUSEBUTTONDOWN:            
 
-                    self.current_dialogue_index += 1 
-                    if self.current_dialogue_index >= len(self.dialogueLines):  # Check if dialogue is over, next chapter
+                    if self.selected_choice_index == 1:
+                        EndingScene(self.screen, "ending-rejected.png", self.currPlayer)
+                        return "main"
+                    elif self.selected_choice_index == 2:
                         self.currPlayer.level = 6  # Proceed to next chapter
                         return "chp"
-                
+                            
+                    self.current_dialogue_index += 1 
                     if not self.show_choices:
                             #if there is a next background index
                         if self.next_background_index:                  
@@ -87,5 +100,6 @@ class Chapter5(Chapter):
                             self.next_background_index = None
                         else:
                             self.current_scene_index += 1  # Proceed to the next background index
+                        
                        
                     self.draw_chapter5()  # Redraw dialogue.
